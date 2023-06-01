c2f5aa5bd82bfd93312e30164ba97e61346684e4
Gourav <gourav@g.com>
2023-06-01 09:48:19 +0530

    Runner class

---
+++ src/test/java/runners/LoginRunner.java
@@ -0,0 +1,24 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		
+		features = {"src\\test\\resources\\applicationtest\\login.feature"},
+		
+		glue = {"stepdefinitions"}
+		
+		
+		
+		
+			
+		
+		)
+
+
+
+
+public class LoginRunner extends AbstractTestNGCucumberTests {
+
+}
