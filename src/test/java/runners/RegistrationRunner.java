f6c5e7d3b7f09fc3f2151d73988510ac1f60e355
Gourav <gourav@g.com>
2023-06-04 09:37:45 +0530

    Scenario outline and Data table

---
+++ src/test/java/runners/RegistrationRunner.java
@@ -0,0 +1,20 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		
+		features = {"src\\test\\resources\\applicationtest\\Registration.feature"},
+		
+		glue = {"stepdefinitions"},
+		
+		plugin = {"pretty"}
+		
+		
+		)
+public class RegistrationRunner extends AbstractTestNGCucumberTests
+{
+	
+
+}
